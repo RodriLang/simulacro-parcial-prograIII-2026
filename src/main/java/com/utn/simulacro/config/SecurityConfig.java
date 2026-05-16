2b8091d0aa8348c58ab7ba4d9f10e7618205c15a
rodrilang <rodrigolang90mail.com>
2026-05-16 18:08:38 -0300

    se agrega dependencia de springSecurity y configuracion básica

---
+++ src/main/java/com/utn/simulacro/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.utn.simulacro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/games").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/members").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
--- src/main/java/com/utn/simulacro/controllers/ReservationController.java
+++ src/main/java/com/utn/simulacro/controllers/ReservationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/reservations")
+@RequestMapping("/api/reservations")
 @RequiredArgsConstructor
 public class ReservationController {
 
